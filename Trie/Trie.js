ea2b3f661da71415730bb500e46ff23470ac8c96
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-10-19 16:55:06 +0530

    feat: function for auto completion

--- Trie/Trie.js
+++ Trie/Trie.js
@@ -1,19 +1,18 @@
-class TrieNode{
+class Node {
     constructor(){
         this.children={}
-        this.isEnd=null
+        this.isEnd=false
     }
 }
 class Trie{
     constructor(){
-        this.root=new TrieNode()
+        this.root=new Node()
     }
     insert(word){
         let node=this.root
         for(let char of word){
-            if(!node.children[char]){
-                node.children[char]=new TrieNode()
-            }
+            if(!node.children[char])
+                node.children[char]=new Node()
             node=node.children[char]
         }
         node.isEnd=true
@@ -21,32 +20,49 @@ class Trie{
     search(word){
         let node=this.root
         for(let char of word){
-            if(node.children[char]){
-                node=node.children[char]
-            }
-            else return false
+            if(!node.children[char])
+                return false
+            node=node.children[char]
         }
-        if(node.isEnd) return true
-        else{
-            return 'partial word is found'
-        }
+        if(!node.isEnd)
+            return 'partial found'
+        else
+            return 'found'
     }
     startWith(word){
         let node=this.root
         for(let char of word){
-            if(node.children[char]){
-                node=node.children[char]
-            }
-            else
+            if(!node.children[char])
                 return false
+            node=node.children[char]
         }
         return true
+    }
+    autoComplete(word){
+        let node=this.root
+        for(let char of word){
+            if(!node.children[char]) return []
+            node=node.children[char]
+        }
+        let results=[]
+        this.collectWords(node,word,results)
+        return results
+    }
+    collectWords(node,word,results){
+        if(node.isEnd) results.push(word)
+        for(let char in node.children){
+            this.collectWords(node.children[char],word+char,results)
+        }
     }
 }
 const trie=new Trie()
 trie.insert('cat')
-trie.insert('car')
-trie.insert('bag')
-console.log(trie.search('bags'))
-console.log(trie.search('car'))
-console.log(trie.startWith('cars'))
+trie.insert('cap')
+trie.insert("can");
+trie.insert("cape");
+trie.insert("car");
+trie.insert("dog");
+
+console.log(trie.search('ca'))
+console.log(trie.startWith('ca'))
+console.log(trie.autoComplete('ca'))
