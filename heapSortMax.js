d8e56d2238ab5b95c9103eac8f03c976be397cff
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-10-21 17:02:54 +0530

    feat: added the minheap sorting

--- heapSortMax.js
+++ heapSortMax.js
@@ -1,11 +1,12 @@
 function sort(arr){
-    const n=arr.length
+    let n=arr.length
+
     for(let i=Math.floor(n/2)-1;i>=0;i--){
         heapify(arr,n,i)
     }
 
     for(let i=n-1;i>0;i--){
-        [arr[0],arr[i]]=[arr[i],arr[0]]
+        [arr[i],arr[0]]=[arr[0],arr[i]]
         heapify(arr,i,0)
     }
     return arr
@@ -17,12 +18,12 @@ function heapify(arr,n,i){
 
     if(left<n && arr[left]>arr[largest]) largest=left
     if(right<n && arr[right]>arr[largest]) largest=right
-    
+
     if(largest!=i){
-        [arr[i],arr[largest]]=[arr[largest],arr[i]]
+        [arr[largest],arr[i]]=[arr[i],arr[largest]]
         heapify(arr,n,largest)
     }
 }
 
-const arr=[23,43,2,21,1,231,32,4]
+let arr=[32,34,34,23,65,87,54,3]
 console.log(sort(arr))
---
+++ heapSortMin.js
@@ -0,0 +1,27 @@
+function sort(arr){
+    let n=arr.length
+    for(let i=Math.floor(n/2)-1;i>=0;i--){
+        heapify(arr,n,i)
+    }
+    for(let i=n-1;i>0;i--){
+        [arr[0],arr[i]]=[arr[i],arr[0]]
+        heapify(arr,i,0)
+    }
+    return arr
+}
+function heapify(arr,n,i){
+ let largest=i
+ let left=2*i+1   
+ let right=2*i+2   
+ 
+ if(left<n && arr[left]<arr[largest]) largest=left
+ if(right<n && arr[right]<arr[largest]) largest=right
+
+ if(largest!=i){
+    [arr[largest],arr[i]]=[arr[i],arr[largest]]
+    heapify(arr,n,largest)
+ }
+}
+
+const arr=[54,23,21,34,65,7,5,3,23]
+console.log(sort(arr))
