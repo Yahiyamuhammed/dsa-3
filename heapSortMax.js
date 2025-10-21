b98681cb81ec4b0347924ea341cc533a0b7b78f4
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-10-21 14:16:30 +0530

    feat: add the maxheap function

---
+++ heapSortMax.js
@@ -0,0 +1,28 @@
+function sort(arr){
+    const n=arr.length
+    for(let i=Math.floor(n/2)-1;i>=0;i--){
+        heapify(arr,n,i)
+    }
+
+    for(let i=n-1;i>0;i--){
+        [arr[0],arr[i]]=[arr[i],arr[0]]
+        heapify(arr,i,0)
+    }
+    return arr
+}
+function heapify(arr,n,i){
+    let largest=i
+    let left=2*i+1
+    let right=2*i+2
+
+    if(left<n && arr[left]>arr[largest]) largest=left
+    if(right<n && arr[right]>arr[largest]) largest=right
+    
+    if(largest!=i){
+        [arr[i],arr[largest]]=[arr[largest],arr[i]]
+        heapify(arr,n,largest)
+    }
+}
+
+const arr=[23,43,2,21,1,231,32,4]
+console.log(sort(arr))
